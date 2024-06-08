b704e91f9551cc6ea20478d36faabc28038aa903
Shiyu Chen <42055092+s3713434@users.noreply.github.com>
2024-06-08 16:12:28 +1000

    OtherCities UI finished

--- src/components/WeatherCard/components/OtherCities/OtherCities.jsx
+++ src/components/WeatherCard/components/OtherCities/OtherCities.jsx
@@ -1,14 +1,20 @@
 import React from 'react'
-import sydney from '../../../../assets/City/Sydney.png'
+import cloudy from '../../../../assets/weather_icon/Cloudy.png'
+import cloudyDay from '../../../../assets/weather_icon/Cloudy_day.png'
+import hail from '../../../../assets/weather_icon/Hail.png'
+import rain from '../../../../assets/weather_icon/Rain.png'
+import snow from '../../../../assets/weather_icon/Snow.png'
+import sunny from '../../../../assets/weather_icon/Sunny.png'
 
 export default function OtherCities() {
   return (
     <ul className='mt-10 flex flex-wrap gap-4'>
-      <li className='relative bg-cover bg-center rounded-2xl overflow-hidden w-64 h-32' style={{ backgroundImage: "url('')" }}>
-        <div className="absolute inset-0 bg-gradient-to-t from-blue-500 to-transparent mix-blend-multiply"></div>
-        <div className="relative z-10 p-4 text-white">
-          <h3 className='text-lg font-bold'>Sydney</h3>
-          <p>28 ~ 32°</p>
+      <li className='relative bg-cover bg-center rounded-2xl overflow-hidden w-48 h-32 flex justify-center items-center' style={{ backgroundImage: "url('https://github.com/s3713434/weather-app/blob/master/src/assets/City/Sydney.png?raw=true')" }}>
+        <div className='absolute inset-0 bg-gradient-to-b from-blue-50 to-blue-500 mix-blend-multiply'></div>
+        <div className='relative z-10 text-white flex flex-col justify-center items-center'>
+          <img className='w-12 h-12 mb-2' src={sunny} alt="Weather Icon" />
+          <h3 className='text-lg font-bold text-xl'>Sydney</h3>
+          <p className='text-sm'>28 ~ 32°</p>
         </div>
       </li>
     </ul>
