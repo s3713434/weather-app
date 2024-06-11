643b08557838579a139f68b728506f80a8b22b52
Shiyu Chen <42055092+s3713434@users.noreply.github.com>
2024-06-11 18:45:40 +1000

    Finishing basic function of SearchBar

--- src/components/WeatherCard/components/SearchBar/SearchBar.jsx
+++ src/components/WeatherCard/components/SearchBar/SearchBar.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export default function SearchBar() {
+export default function SearchBar({ onSearch }) {
+  const [cityName, setCityName] = useState('')
+
+  const handleChange = (e) => {
+    onSearch(cityName)
+    setCityName('')
+  }
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      handleChange()
+    }
+  }
+
   return (
     <div className='bg-white p-1 w-3/5 flex space-x-1 rounded-2xl mt-20'>
       <input
         className='w-full p-2 outline-none'
         type='text'
         placeholder='Search for a city'
+        value={cityName}
+        onChange={(e) => setCityName(e.target.value)}
+        onKeyDown={handleKeyDown}
       />
       <button
         className='bg-indigo-600 text-white py-2 px-5 rounded-2xl cursor-pointer hover:bg-indigo-700'
         type='button'
+        onClick={handleChange}
       >
         Search
       </button>
